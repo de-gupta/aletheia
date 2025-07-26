f7ed417be479e81fa21ea8f531cae77ae71105c8
de-gupta <de-gupta>
2025-07-27 00:23:18 +0200

    pair class

--- src/main/java/de/gupta/aletheia/collection/Pair.java
+++ src/main/java/de/gupta/aletheia/collection/Pair.java
@@ -1,9 +1,21 @@
 package de.gupta.aletheia.collection;
 
+import java.util.function.Function;
+
 public record Pair<A, B>(A first, B second)
 {
+	public <C> Pair<C, B> transformFirst(final Function<A, C> transformation)
+	{
+		return Pair.of(transformation.apply(first), second);
+	}
+
 	public static <A, B> Pair<A, B> of(A first, B second)
 	{
 		return new Pair<>(first, second);
+	}
+
+	public <C> Pair<A, C> transformSecond(final Function<B, C> transformation)
+	{
+		return Pair.of(first, transformation.apply(second));
 	}
 }
