ebeb67064c9b25141d9330832319caf46efa61d1
de-gupta <de-gupta>
2025-09-29 09:31:53 +0200

    harbors test
    Took 48 seconds

--- src/test/java/de/gupta/aletheia/collection/crucible/HarborsTest.java
+++ src/test/java/de/gupta/aletheia/collection/crucible/HarborsTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -126,6 +127,86 @@ final class HarborsTest
 			assertThat(emptyRelic.harbors(null))
 					.as("Empty relic should not harbor null")
 					.isFalse();
+		}
+	}
+
+	private static final class CaseInsensitiveString
+	{
+		private final String value;
+
+		@Override
+		public int hashCode()
+		{
+			return value.toLowerCase().hashCode();
+		}
+
+		@Override
+		public boolean equals(final Object other)
+		{
+			if (this == other) return true;
+			if (other == null || getClass() != other.getClass()) return false;
+			final var that = (CaseInsensitiveString) other;
+			return value.equalsIgnoreCase(that.value);
+		}
+
+		@Override
+		public String toString()
+		{
+			return value;
+		}
+
+		CaseInsensitiveString(final String value)
+		{
+			this.value = Objects.requireNonNull(value);
+		}
+	}
+
+	@Nested
+	@DisplayName("Custom equals method behavior")
+	final class CustomEqualsTests
+	{
+		@ParameterizedTest(name = "{0}")
+		@MethodSource("crucibleFactories")
+		@DisplayName("Should respect custom equals implementation for case insensitive strings")
+		void testHarborsWithCaseInsensitiveStrings(final String description,
+												   final Function<List<CaseInsensitiveString>, Crucible<CaseInsensitiveString>> crucibleFactory)
+		{
+			final var elements = List.of(
+					new CaseInsensitiveString("hello"),
+					new CaseInsensitiveString("world"),
+					new CaseInsensitiveString("test")
+			);
+			final var crucible = crucibleFactory.apply(elements);
+
+			assertThat(crucible.harbors(new CaseInsensitiveString("HELLO")))
+					.as("Crucible should harbor case insensitive match for hello")
+					.isTrue();
+
+			assertThat(crucible.harbors(new CaseInsensitiveString("Hello")))
+					.as("Crucible should harbor mixed case match for hello")
+					.isTrue();
+
+			assertThat(crucible.harbors(new CaseInsensitiveString("WORLD")))
+					.as("Crucible should harbor case insensitive match for world")
+					.isTrue();
+
+			assertThat(crucible.harbors(new CaseInsensitiveString("missing")))
+					.as("Crucible should not harbor absent element")
+					.isFalse();
+
+			assertThat(crucible.harbors(new CaseInsensitiveString("MISSING")))
+					.as("Crucible should not harbor absent element even with different case")
+					.isFalse();
+		}
+
+		private static Stream<Arguments> crucibleFactories()
+		{
+			return Stream.of(
+					Arguments.of("Forge with case insensitive strings",
+							(Function<List<CaseInsensitiveString>, Crucible<CaseInsensitiveString>>) Forge::kindle),
+					Arguments.of("Relic with case insensitive strings",
+							(Function<List<CaseInsensitiveString>, Crucible<CaseInsensitiveString>>) Relic::consecrate)
+			);
 		}
 	}
 }
