6014935839c5d7b1ad30518754dea4e6e485a229
de-gupta <de-gupta>
2026-03-18 11:55:40 +0100

    params final

--- src/main/java/de/gupta/aletheia/collection/folding/Loom.java
+++ src/main/java/de/gupta/aletheia/collection/folding/Loom.java
@@ -8,13 +8,13 @@ import java.util.function.BiFunction;
 @FunctionalInterface
 public interface Loom<E>
 {
-	static <E> Loom<E> harness(Iterable<? extends E> iterable)
+	static <E> Loom<E> harness(final Iterable<? extends E> iterable)
 	{
 		Objects.requireNonNull(iterable);
 		return new Loom<>()
 		{
 			@Override
-			public <R> R weave(R initial, BiFunction<? super R, ? super E, ? extends R> operation)
+			public <R> R weave(final R initial, final BiFunction<? super R, ? super E, ? extends R> operation)
 			{
 				R result = initial;
 				for (E e : iterable)
@@ -26,9 +26,9 @@ public interface Loom<E>
 		};
 	}
 
-	<R> R weave(R initial, BiFunction<? super R, ? super E, ? extends R> operation);
+	<R> R weave(final R initial, final BiFunction<? super R, ? super E, ? extends R> operation);
 
-	default Unfolding<E> forge(BiFunction<? super E, ? super E, ? extends E> operation)
+	default Unfolding<E> forge(final BiFunction<? super E, ? super E, ? extends E> operation)
 	{
 		return weave(Unfolding.chaos(), (acc, e) ->
 				acc.sterile() ? Unfolding.beckon(e) : acc.conjoin(e, operation));
